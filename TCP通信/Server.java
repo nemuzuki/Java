175b1ddd441daa98441385bd2aa86859908bb17e
Mikazuki <44475022+3rdtsuki@users.noreply.github.com>
2021-12-07 13:25:04 +0800

    Create Server.java

---
+++ TCP通信/Server.java
@@ -0,0 +1,37 @@
+package hello;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server{
+	public static void main(String[] args) throws IOException {
+		ServerSocket ss=new ServerSocket(6666);//服务端的端口号是6666
+		System.out.println("begin listen");
+		Socket s=ss.accept();
+		System.out.println("receive client"+s);
+		
+		BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
+		PrintWriter out=new PrintWriter(s.getOutputStream(),true);//输出流
+		ReceiveThread receiveThread=new ReceiveThread(in);
+		BufferedReader stdin=new BufferedReader(new InputStreamReader(System.in));
+		
+		receiveThread.start(); 
+		String message;
+		while(true) {
+			message = stdin.readLine();
+			out.println(message);//向对方发送信息
+			if(message.equals("q")) {
+				break;
+			}
+		}
+		
+		in.close();
+		out.close();
+		s.close();
+		ss.close();
+	}
+}
